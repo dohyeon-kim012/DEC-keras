22c7f5ef32eb1553b58bdd7c6c5ca8847b50d730
dohyeon-kim012 <dohyeon.kim.012@gmail.com>
2021-10-12 18:13:46 +0900

    changed

--- DEC-keras/datasets.py
+++ DEC-keras/datasets.py
@@ -316,7 +316,7 @@ def load_crawling_data():
     okt = Okt()
     from tensorflow.keras.preprocessing.text import Tokenizer
     tokenizer = Tokenizer()
-
+    from transformers import BertTokenizer
     from gensim.models import Word2Vec
     tokenized_data = []
     y_list = []
@@ -347,28 +347,43 @@ def load_crawling_data():
     # 데이터가 이미 형태소 분석이 되어 있는 경우
 
     print('Loading data...')
+    # df1 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_1.csv", index_col=[0])
+    # df2 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_2.csv", index_col=[0])
+    # df3 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_3.csv", index_col=[0])
+    # df4 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_4.csv", index_col=[0])
+    # df5 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_5.csv", index_col=[0])
+    # df = pd.concat([df1[:30000], df2[:30000], df3[:30000], df4[:30000], df5[:30000]])
+    # df = df.loc[:, ['total_score', 'tokenized_review']]
+    # df_dropped = df.dropna(axis=0)
+    # df_shuffled = df_dropped.sample(frac=1).reset_index(drop=True)
+    # print(df_shuffled)
+
     df1 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_1.csv", index_col=[0])
-    df2 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_2.csv", index_col=[0])
-    df3 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_3.csv", index_col=[0])
-    df4 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_4.csv", index_col=[0])
+    # df2 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_2.csv", index_col=[0])
+    # df3 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_3.csv", index_col=[0])
+    # df4 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_4.csv", index_col=[0])
     df5 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_5.csv", index_col=[0])
     # 점수별로 나뉘어져 있는 df에서, 개수만 임의로 조정
-    df = pd.concat([df1[:30000], df2[:30000], df3[:30000], df4[:30000], df5[:30000]])
+    df = pd.concat([df1[:30000], df5[:30000]])  # , df2[:30000], df3[:30000], df4[:30000],
     df = df.loc[:, ['total_score', 'tokenized_review']]
     df_dropped = df.dropna(axis=0)
+    data = df_dropped.sample(frac=1).reset_index(drop=True)
     df_shuffled = df_dropped.sample(frac=1).reset_index(drop=True)
-    print(df_shuffled)
-    x_list = df_shuffled['tokenized_review'].values.tolist()
-    data = []
-    for i in tqdm(range(len(x_list)), desc='okt 리뷰 data >>> pkl list data'):
-        x_list[i] = x_list[i].replace("[", "")
-        x_list[i] = x_list[i].replace("]", "")
-        x_list[i] = x_list[i].replace(",", "")
-        x_list[i] = x_list[i].replace("'", "")
-        x_list[i] = x_list[i].split()
+    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
+    tokenized_texts = [tokenizer.tokenize(s) for s in data['preprocessed_review']]
+    # 점수별로 나뉘어져 있는 df에서, 개수만 임의로 조정
 
-        data.append(x_list[i])
-    print(data[149921])
+    # x_list = df_shuffled['tokenized_review'].values.tolist()
+    # data = []
+    # for i in tqdm(range(len(x_list)), desc='okt 리뷰 data >>> pkl list data'):
+    #     x_list[i] = x_list[i].replace("[", "")
+    #     x_list[i] = x_list[i].replace("]", "")
+    #     x_list[i] = x_list[i].replace(",", "")
+    #     x_list[i] = x_list[i].replace("'", "")
+    #     x_list[i] = x_list[i].split()
+    #
+    #     data.append(x_list[i])
+    # print(data[149921])
     # df = pd.read_csv("./reviewdata_hotelsdotcom_okt.csv", index_col=[0], nrows=300)
     # df.dropna(subset=['tokenized_review', 'total_score'], inplace=True)
     # df = df_shuffled.reset_index(drop=True)
@@ -405,14 +420,15 @@ def load_crawling_data():
 
     # vocab_size = total_cnt - rare_cnt + 1
     # print(vocab_size)
-
+    #
     # tokenizer = Tokenizer(vocab_size, oov_token='<oov>')
     # tokenizer.fit_on_texts(tokenized_data)
     # x = tokenizer.texts_to_matrix(tokenized_data, mode='tfidf')
 
+
     # Word2Vec 시도
-    min_count = 10
-    model = Word2Vec(sentences=data, window=5, min_count=min_count, workers=4, sg=1)
+    min_count = 5
+    model = Word2Vec(sentences=tokenized_texts, window=5, min_count=min_count, workers=4, sg=1)
     print(f'Vectorizing sequence data with min_count {min_count}...')
     # model = Word2Vec.load('model_mincnt_20')
     # x = np.zeros((len(tokenized_data), 100), dtype='float32')
@@ -460,6 +476,218 @@ def load_crawling_data():
     return x.astype(float), y, raw_data
 
 
+def load_crawling_data_fasttext():
+    from konlpy.tag import Okt
+    okt = Okt()
+    from tensorflow.keras.preprocessing.text import Tokenizer
+    tokenizer = Tokenizer()
+
+    from gensim.models import Word2Vec, FastText
+    tokenized_data = []
+    y_list = []
+    import pandas as pd
+    import gzip
+    import pickle
+
+    '''형태소분석 필요한 데이터의 경우
+    print('Loading data...')
+    df = pd.read_csv("./data/crawling_data/reviewdata_hotelsdotcom_preprocessed.csv", index_col=[0], nrows=50000)
+    df.dropna(subset=['preprocessed_review', 'total_score'], inplace=True)
+    df = df.reset_index(drop=True)
+    data = df['preprocessed_review']
+    y = (df['total_score'] / 2)
+
+    num_classes = np.max(y)
+    print(num_classes, 'classes')
+
+    print('Vectorizing sequence data...')
+    for review in data:
+        temp = []
+        oks = okt.pos(review, join=True)
+        for ok in oks:
+            text, tag = map(str, ok.split('/'))
+            if tag not in ['Josa', 'Eomi', 'Punctation']:
+                temp.append(text)
+        tokenized_data.append(temp)
+    '''
+    # 데이터가 이미 형태소 분석이 되어 있는 경우
+
+    print('Loading data...')
+    df1 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_1.csv", index_col=[0])
+    # df2 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_2.csv", index_col=[0])
+    # df3 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_3.csv", index_col=[0])
+    # df4 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_4.csv", index_col=[0])
+    df5 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_5.csv", index_col=[0])
+    # 점수별로 나뉘어져 있는 df에서, 개수만 임의로 조정
+    df = pd.concat([df1[:30000], df5[:30000]])  # , df2[:30000], df3[:30000], df4[:30000],
+    df = df.loc[:, ['total_score', 'tokenized_review']]
+    df_dropped = df.dropna(axis=0)
+    df_shuffled = df_dropped.sample(frac=1).reset_index(drop=True)
+    data = df_shuffled['tokenized_review']
+    # print(df_shuffled)
+    # x_list = df_shuffled['tokenized_review'].values.tolist()
+    # data = []
+    # for i in tqdm(range(len(x_list)), desc='okt 리뷰 data >>> pkl list data'):
+    #     x_list[i] = x_list[i].replace("[", "")
+    #     x_list[i] = x_list[i].replace("]", "")
+    #     x_list[i] = x_list[i].replace(",", "")
+    #     x_list[i] = x_list[i].replace("'", "")
+    #     x_list[i] = x_list[i].split()
+    #
+    #     data.append(x_list[i])
+    # print(data[149921])
+    # df = pd.read_csv("./reviewdata_hotelsdotcom_okt.csv", index_col=[0], nrows=300)
+    # df.dropna(subset=['tokenized_review', 'total_score'], inplace=True)
+    # df = df_shuffled.reset_index(drop=True)
+    # tokenized_data = df['tokenized_review']
+    # y = (df['total_score'] / 2)
+    y = np.array(df_shuffled['total_score'], dtype='int64')
+    raw_data = []
+    # for num in range(1, 5):
+    # with gzip.open('./data/crawling_data/pkl_list_review_data_by_total_score_4.pkl', 'rb') as f:
+    #     data = pickle.load(f)
+    #     data = data[:y.size]
+
+    num_classes = np.unique(y)
+    print(num_classes, 'classes')
+
+    min_count = 10
+    model = FastText(sentences=data, window=5, min_count=min_count, workers=4, sg=1)
+    # model = FastText.load_fasttext_format('./cc.ko.300.bin.gz')
+    print(f'Vectorizing sequence data with min_count {min_count}...')
+    # model = Word2Vec.load('model_mincnt_20')
+    # x = np.zeros((len(tokenized_data), 100), dtype='float32')
+    x = np.empty((300,), dtype='float32')
+    nwords = 0.
+    # counter = 0.
+    # GPU ver.
+    idx_to_key = model.wv.index2word
+    # local ver.
+    # idx_to_key = model.wv.index_to_key
+    # key_to_idx = model.wv.key_to_index
+    index2word_set = set(idx_to_key)
+    for idx in tqdm(range(len(data)), desc="Word2Vec"):
+        featureVec = np.zeros((300,), dtype='float32')
+        for word in data[idx]:
+            if word in index2word_set:
+                nwords = nwords + 1.
+                # print('len :', len(model.wv[word])) ---> 100
+                featureVec = np.add(featureVec, model.wv[word])
+                # featureVec[np.arange(featureVec, model.wv[word])] = 1   --> 실패!
+                # featureVec = to_categorical(model.wv[word], num_classes=(len(model.wv[word])))
+        # x값이 빈 배열일 때는 y와 raw_data도 패스
+        is_all_zero = not np.any(featureVec)
+        if is_all_zero:
+            pass
+        else:
+            featureVec = np.divide(featureVec, nwords)
+            # print('featureVec :', featureVec)
+            x = np.append(x, featureVec, axis=0)
+            # y의 개수를 x만큼 맞추기 위해
+            # y_list.append(y[int(counter)])
+            y_list.append(y[idx])
+            review = []
+            review.append(df_shuffled['review'][idx])
+            review.append(df_shuffled['tokenized_review'][idx])
+            raw_data.append(review)
+        # counter += 1
+            # print('x :', counter, x[int(counter)])
+    # x = to_categorical(x, num_classes=x)
+    y = np.array(y_list, dtype='int64')
+    x = x[300:]
+    x = x.reshape(y.size, -1)
+    print(len(y), 'train sequences')
+    print('x_train shape:', x.shape)
+    print('raw_review_data shape:', len(raw_data))
+    print('y shape:', y.shape)
+
+    return x.astype(float), y, raw_data
+
+
+def load_crawling_data_bert():
+    from transformers import BertTokenizer
+    import pandas as pd
+
+    df1 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_1.csv", index_col=[0])
+    # df2 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_2.csv", index_col=[0])
+    # df3 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_3.csv", index_col=[0])
+    # df4 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_4.csv", index_col=[0])
+    df5 = pd.read_csv("./data/crawling_data/reviewdata_total_labeled_5.csv", index_col=[0])
+    # 점수별로 나뉘어져 있는 df에서, 개수만 임의로 조정
+    df = pd.concat([df1[:30000], df5[:30000]])  # , df2[:30000], df3[:30000], df4[:30000],
+    df = df.loc[:, ['total_score', 'tokenized_review']]
+    df_dropped = df.dropna(axis=0)
+    data = df_dropped.sample(frac=1).reset_index(drop=True)
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
+    tokenized_texts = [tokenizer.tokenize(s) for s in data['preprocessed_review']]
+
+
+    
+
+def data_for_pretrain():
+    import pandas as pd
+    # from konlpy.tag import Okt
+    # okt = Okt()
+    # from tensorflow.keras.preprocessing.text import Tokenizer
+    # tokenizer = Tokenizer()
+    from gensim.models import Word2Vec, FastText
+    tokenized_data = []
+    y_list = []
+    import pandas as pd
+    import gzip
+    import pickle
+
+    # 데이터가 이미 형태소 분석이 되어 있는 경우
+
+    print('Loading data for pretrain...')
+    cleandata = pd.read_csv("cleandata_labeled_1+5.csv", encoding='utf-8')
+    df_shuffled = cleandata.sample(frac=1).reset_index(drop=True)
+    data = df_shuffled['tokenized_review']
+    y = np.array(df_shuffled['total_score'], dtype='int64')
+    raw_data = []
+
+    num_classes = np.max(y)
+    print(num_classes, 'classes')
+
+    min_count = 10
+    # model = FastText(sentences=data, window=5, min_count=min_count, workers=4, sg=1)
+    model = FastText.load_fasttext_format('./cc.ko.300.bin.gz')
+    print(f'Vectorizing sequence data with min_count {min_count}...')
+    x = np.empty((300,), dtype='float32')
+    nwords = 0.
+    # GPU ver.
+    idx_to_key = model.wv.index2word
+    # local ver.
+    # idx_to_key = model.wv.index_to_key
+    # key_to_idx = model.wv.key_to_index
+    index2word_set = set(idx_to_key)
+    for idx in tqdm(range(len(data)), desc="Word2Vec"):
+        featureVec = np.zeros((300,), dtype='float32')
+        for word in data[idx]:
+            if word in index2word_set:
+                nwords = nwords + 1.
+                featureVec = np.add(featureVec, model.wv[word])
+        is_all_zero = not np.any(featureVec)
+        if is_all_zero:
+            pass
+        else:
+            featureVec = np.divide(featureVec, nwords)
+            x = np.append(x, featureVec, axis=0)
+            y_list.append(y[idx])
+            raw_data.append(df_shuffled['tokenized_review'][idx])
+    y = np.array(y_list, dtype='int64')
+    x = x[300:]
+    x = x.reshape(y.size, -1)
+    print(len(y), 'train sequences')
+    print('X_DATA for pretrain:', x.shape)
+    print('raw_review_data shape:', len(raw_data))
+    print('Y_DATA for pretrain:', y.shape)
+
+    return x.astype(float), y
+
+
+
 def load_data(dataset_name):
     if dataset_name == 'mnist':
         return load_mnist()
@@ -477,6 +705,8 @@ def load_data(dataset_name):
         return load_imdb()
     elif dataset_name == 'crawling_data':
         return load_crawling_data()
+    elif dataset_name == 'crawling_data_fasttext':
+        return load_crawling_data_fasttext()
     else:
         print('Not defined for loading', dataset_name)
         exit(0)
